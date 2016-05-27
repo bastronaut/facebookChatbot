8882246ca0ed68ac1282ee5c1fecfc7feb1d72fd
bastronaut <btsijtsma@gmail.com>
2016-05-27 19:25:26 +0200

    did add vertex, remove vertex and is_adjacent

--- chatbot/responseBuilderGraph.py
+++ chatbot/responseBuilderGraph.py
@@ -5,6 +5,8 @@ graph. Also using pep standard from now on.
 Start building all graph methods, then probably abstract them away
 with chatbot behavior calls. Will use adjacency list for this.
 https://en.wikipedia.org/wiki/Adjacency_list
+Adjacency list is an unordered list of nodes with a set of neighbouring nodes.
+graph g = { a : (b, c), b : (a), c: (a)}
 
 General graph methods:
 adjacent(G, x, y): tests whether there is an edge from the vertices x to y;
@@ -16,15 +18,22 @@ remove_edge(G, x, y): removes the edge from  vertices x to y, if it is there;
 get_vertex_value(G, x): returns the value associated with the vertex x;
 set_vertex_value(G, x, v): sets the value associated with the vertex x to v.
 '''
+from sets import Set
 
 
 class ResponseBuilderGraph:
+    conversations = {}
 
     def is_adjacent(self, x, y):
-        return
+        return y in conversations[x]
 
     def add_vertex(self, x):
-        return
+        if x in conversations:
+            return
+        else:
+            conversations[x] = Set([])
 
     def remove_vertex(self, x):
-        return
+        if x in conversations:
+            del conversations[x]
+        
